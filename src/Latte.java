3a3134d6d6691de6e0421fba7511a477fde68a5d
Piero Araujo <pieritoeeuu@gmail.com>
2026-07-22 20:41:09 -0500

    feat: Crear la clase Latte

---
+++ src/Latte.java
@@ -0,0 +1,12 @@
+public class Latte implements Cafe{
+
+    @Override
+    public String getDescripcion() {
+        return "Latte";
+    }
+
+    @Override
+    public double getCosto() {
+        return 4;
+    }
+}
