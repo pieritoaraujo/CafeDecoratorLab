a043acd87abb2e54e293baafc4cb1b24aeab9372
Piero Araujo <pieritoeeuu@gmail.com>
2026-07-22 20:42:30 -0500

    feat: Crear la clase Cappuchino

---
+++ src/Cappuchino.java
@@ -0,0 +1,11 @@
+public class Cappuchino implements Cafe{
+    @Override
+    public String getDescripcion() {
+        return "Cappuchino";
+    }
+
+    @Override
+    public double getCosto() {
+        return 8;
+    }
+}
