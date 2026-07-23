bdb54334a3871476d0ff2590cab763e4939556ec
Piero Araujo <pieritoeeuu@gmail.com>
2026-07-22 20:49:14 -0500

    feat: Agregar el decorador Canela

---
+++ src/Canela.java
@@ -0,0 +1,16 @@
+public class Canela extends DecoratorCafe{
+
+    public Canela(Cafe cafe){
+        super(cafe);
+    }
+
+    @Override
+    public String getDescripcion() {
+        return cafe.getDescripcion() + "con Canela";
+    }
+
+    @Override
+    public double getCosto() {
+        return cafe.getCosto() + 0.5;
+    }
+}
